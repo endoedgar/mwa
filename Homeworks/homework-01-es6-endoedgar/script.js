d7c792c7c060ef09111202a2068d97643af31681
Edgar de Jesus Endo Junior <endoedgarjunior@gmail.com>
2020-02-25 16:02:34 -0600

    Changed http api to https

--- Homeworks/homework-01-es6-endoedgar/script.js
+++ Homeworks/homework-01-es6-endoedgar/script.js
@@ -2,7 +2,7 @@ const { fromEvent, timer, from, zip, of } = rxjs;
 const { flatMap, map, skip, switchMap, catchError } = rxjs.operators;
 const { fromFetch } = rxjs.fetch;
 
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 const timerElement = document.getElementById('timer')
