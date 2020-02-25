020a295385fa9bff4e1a7b77d960a52a22114f59
Edgar de Jesus Endo Junior <endoedgarjunior@gmail.com>
2020-02-25 13:32:30 -0600

    Added homework-01

---
+++ Homeworks/homework-01-es6-endoedgar/README.md
@@ -0,0 +1,11 @@
+# MWA Homework 01 - ECMAScript 2015 (ES6)
+## Exercise - Reactive Game
+Use RXJS and convert all events, timers, ajax calls, arrays to Observables for the following Speed-Typing game.
+  * Convert events to Observables
+  * Convert timers to Observables
+  * Convert all arrays to Observables
+  * Convert Fetch from: [Quotable API](http://api.quotable.io/random) to Observable
+  * Update the code so a new quote will be fetched when solution is correct or timer has reached 100 seconds
+  
+    
+![Screenshot](./screenshot.png)
---
+++ Homeworks/homework-01-es6-endoedgar/script.js
@@ -0,0 +1,80 @@
+const { fromEvent, timer, from, zip, of } = rxjs;
+const { flatMap, map, skip, switchMap, catchError } = rxjs.operators;
+const { fromFetch } = rxjs.fetch;
+
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
+
+let timer$;
+
+fromEvent(quoteInputElement, 'input').subscribe(() => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+  const arrayValue = quoteInputElement.value.padEnd(arrayQuote.length, '\0').split('')
+  let correct = true
+
+  zip(from(arrayQuote), from(arrayValue)).pipe(
+    map(([characterSpan, character]) => ({characterSpan, character}))
+  ).subscribe(
+    ({characterSpan, character}) => {
+      if (character == '\0') {
+        characterSpan.classList.remove('correct');
+        characterSpan.classList.remove('incorrect');
+        correct = false
+      } else if (character === characterSpan.innerText) {
+        characterSpan.classList.add('correct');
+        characterSpan.classList.remove('incorrect');
+      } else {
+        characterSpan.classList.add('incorrect');
+        characterSpan.classList.remove('correct');
+        correct = false;
+      }
+    }
+  );
+
+  if (correct) renderNewQuote()
+});
+
+function getRandomQuote() {
+  return fromFetch(RANDOM_QUOTE_API_URL).pipe(
+    switchMap(response => 
+      (response.ok) ? response.json() : of({error: true, message: `Error ${response.status}`})
+    ),
+    catchError(err => {
+      return of({ error: true, message: err.message });
+    }),
+    map(
+      quoteResult => quoteResult.content
+    )
+  );
+}
+
+function renderNewQuote() {
+  quoteDisplayElement.innerHTML = '';
+  getRandomQuote().pipe(
+    flatMap(quote => quote.split(''))
+  ).subscribe(
+    character => {
+      const characterSpan = document.createElement('span');
+      characterSpan.innerText = character;
+      quoteDisplayElement.appendChild(characterSpan);
+    },
+    e => console.error(e),
+    () => {
+      quoteInputElement.value = null;
+      startTimer();
+    }
+  )
+}
+
+function startTimer() {
+  if(typeof timer$ !== 'undefined') timer$.unsubscribe();
+  
+  timer$ = timer(0, 1000).subscribe(time => {
+    timerElement.innerText = time
+    if(time == 100) renderNewQuote();
+  });
+};
+
+renderNewQuote()
